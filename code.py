3081907ab6fdf44849389dbf46623a768d423a64
XiR0 <106984734+XiRO0O@users.noreply.github.com>
2022-06-09 22:24:14 +0200

    adding exponentation function

--- code.py
+++ code.py
@@ -10,17 +10,21 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def exponentation(x, y):
+    return x ** y
+
 print("select operation:")
 print("1.add")
 print("2.subtract")
 print("3.multiply")
 print("4.divide")
+print("5.exponentation")
 
 while True:
     
-    choice = input("enter choice(1/2/3/4): ")
+    choice = input("enter choice(1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("enter first number: "))
         num2 = float(input("enter second number: "))
 
@@ -35,6 +39,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+            
+        elif choice == '4':
+            print(num1, "^", num2, "=", exponentation(num1, num2))
         
         next_calculation = input("let's do next calculation? (y/n): ")
         if next_calculation == "n":
